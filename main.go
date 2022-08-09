cf4c5942cc5c7a22109a08c9eb760e95fc9fab77
maru44 <takumaru22244@gmail.com>
2022-08-09 19:58:49 +0900

    minimum

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/skip2/go-qrcode"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Println("need one or two args")
+	}
+
+	fileName := "qr.png"
+	if len(args) > 1 {
+		fileName = args[1]
+	}
+
+	qrcode.WriteFile(args[0], qrcode.Medium, 256, fileName)
+}
